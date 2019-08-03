bafdd7070a4b459f56ffd227db0e829630b5900c
bee25141 <bee25141@gmail.com>
2019-08-03 16:27:11 -0500

    updated linkedin fa icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
                 <div className="inner">
                     <ul className="icons">
                         <li><a href="https://github.com/bee25141" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="https://www.linkedin.com/in/tony-bee-abab93174/" className="icon fa-linkedin-in"><span className="label">LinkedIn</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/tony-bee-abab93174/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
                     </ul>
                 </div>
             </div>
