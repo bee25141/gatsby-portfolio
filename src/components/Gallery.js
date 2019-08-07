f0b0709ec39dbcb33108a072d94fc4997cf5263d
bee25141 <bee25141@gmail.com>
2019-08-07 14:04:42 -0500

    working on linking to github on click

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -31,6 +31,7 @@ class Gallery extends Component {
                         onClick={e => {
                             e.preventDefault();
                             // this.toggleLightbox(i);
+                            <a href={this.obj.source} />
                         }}
                     >
                         <img src={obj.thumbnail} />
