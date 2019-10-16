6e04db70d4975f10b4aa676d4a8bb0276ef07d61
bee25141 <bee25141@gmail.com>
2019-10-16 12:49:28 -0500

    updated clicky game desc.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ import liri from '../assets/images/thumbs/liri.png'
 
 const DEFAULT_IMAGES = [
     { id: '1', source: "https://github.com/bee25141/lunch-calculator", thumbnail: lunch_calculator, caption: 'Lunchbox Chi', description: "Finding the best 'bang for your buck' lunch in Chicago (Under Construction)"},
-    { id: '2', source: "https://github.com/bee25141/Clicky-Game", thumbnail: clicky_game, caption: 'Clicky Game', description: 'React memory game'},
+    { id: '2', source: "https://github.com/bee25141/Clicky-Game", thumbnail: clicky_game, caption: 'Clicky Game', description: 'React.js memory game deployed using Docker & AWS'},
     { id: '3', source: "https://github.com/bee25141/FriendFinder", thumbnail: friend_finder, caption: 'Friend Finder', description: 'Web app to fiend friends with simmilar personalities'},
     { id: '4', source: "https://github.com/bee25141/bamazon", thumbnail: bamazon, caption: 'Bamazon', description: 'Command line storefront application utilizing MySql'},
     { id: '5', source: "https://github.com/bee25141/liri-node-app", thumbnail: liri, caption: 'Liri Node App', description: 'Command line node app that returns data for movies, concerts and songs'},
